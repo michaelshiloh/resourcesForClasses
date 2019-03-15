7dd1a5f05dfc0654e0a0917a5f0abe5480367692
Michael Shiloh <michael@michaelshiloh.com>
2019-03-15 18:09:56 +0400

    removed tcpAndHttpServer since it is not working

--- src/nodePrograms/tcpAndHttpServer/arduinoTCPClient/arduino_secrets.h
+++
@@ -1,5 +0,0 @@
-#define SECRET_SSID "IMNetwork"
-#define SECRET_PASS "static123"
-
-//#define SECRET_SSID "NETGEAR55"
-//#define SECRET_PASS "bravecomet559"
--- src/nodePrograms/tcpAndHttpServer/public/client.js
+++
@@ -1,8 +0,0 @@
-function ledON() {
-    console.log ("sending a message to turn on the LED");
-}
-
-function ledOFF() {
-    console.log ("sending a message to turn off the LED");
-}
-
