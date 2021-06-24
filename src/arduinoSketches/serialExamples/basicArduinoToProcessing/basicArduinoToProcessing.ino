bb7440f3532410179900ecaf2303eea3363b9f16
Michael Shiloh <michael@michaelshiloh.com>
2021-06-24 16:56:12 +0400

    added handshaking

--- src/arduinoSketches/serialExamples/basicArduinoToProcessing/basicArduinoToProcessing.ino
+++ src/arduinoSketches/serialExamples/basicArduinoToProcessing/basicArduinoToProcessing.ino
@@ -1,32 +1,46 @@
 /*
   Send a value to Processing.
-  Based on Graph by David A. Mellis
+
+  Based on
+  https://github.com/aaronsherwood/introduction_interactive_media/blob/master/processingExamples/gravityExamples/gravityWind/gravityWind.pde
+  by Aaron Sherwood
 */
+
+const int AINPIN = A2;
 
 void setup() {
   // initialize the serial communication:
   Serial.begin(9600);
+
+  // Since both sides wait for each other before they send anything,
+  // someone needs to start the conversation
+  Serial.println("0");
 }
 
 void loop() {
-  // send the value of analog input 0:
-  Serial.println(analogRead(A0));
-  // wait a bit for the analog-to-digital converter to stabilize after the last
-  // reading:
-  delay(1);
+  while (Serial.available()) {
+    Serial.parseInt(); // no need to store the value since we don't need it
+
+    // Only proceed if we have the end of line
+    if (Serial.read() == '\n') {
+
+      // send the value of analog input 0:
+      Serial.println(analogRead(AINPIN));
+    }
+  }
 }
 
 /* Processing code for this example
 
-// Simple movement of ellipse based on values from Arduino
-// Based on Graphing example by Tom Igoe
+  // Simple movement of ellipse based on values from Arduino
+  // Based on Graphing example by Tom Igoe
 
-import processing.serial.*;
+  import processing.serial.*;
 
-Serial myPort;        // The serial port
-float inByte = 0;
+  Serial myPort;        // The serial port
+  float inByte = 0;
 
-void setup () {
+  void setup () {
   size(400, 300);
 
   // List all the available serial ports
@@ -39,20 +53,20 @@ void setup () {
 
   // don't generate a serialEvent() unless you get a newline character:
   myPort.bufferUntil('\n');
-}
+  }
 
-void draw () {
+  void draw () {
   background(200);
 
   circle(inByte, height/2, 30);
-}
+  }
 
-void serialEvent (Serial myPort) {
+  void serialEvent (Serial myPort) {
   // get the ASCII string:
   String inString = myPort.readStringUntil('\n');
 
   // Always check to make sure the string isn't empty
-  if (inString != null) { 
+  if (inString != null) {
     // trim off any whitespace:
     inString = trim(inString);
     // convert to a float
@@ -60,6 +74,9 @@ void serialEvent (Serial myPort) {
     // map to the width of the screen
     inByte = map(inByte, 0, 1023, 0, width);
     println(inByte);
+
+    // Tell Arduino we're ready for another
+    myPort.write(1 + "\n"); // could be any value
   }
 }
 */
