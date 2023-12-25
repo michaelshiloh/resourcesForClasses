a4e167038f69efbfe6a2a88124493d294c1b6556
Michael Shiloh <michael@michaelshiloh.com>
2023-12-25 11:26:58 +0400

    added ASMC datasheet

--- src/arduinoSketches/hobbyRC/minimalMoreChannels/minimalMoreChannels.ino
+++ src/arduinoSketches/hobbyRC/minimalMoreChannels/minimalMoreChannels.ino
@@ -20,7 +20,7 @@
 #include <EnableInterrupt.h>
 
 #define SERIAL_PORT_SPEED 9600
-#define RC_NUM_CHANNELS  4
+#define RC_NUM_CHANNELS  5
 
 // DO NOT CHANGE THESE NUMBERS - THEY ARE 
 // INDEXES INTO THE ARRAY
@@ -28,13 +28,17 @@
 #define RC_CH2  1
 #define RC_CH3  2
 #define RC_CH4  3
+#define RC_CH5  4
+#define RC_CH6  5
 
 // CHANGE THESE NUMBERS TO CORRESPOND TO THE PINS THAT
 // YOUR SHIELD HAS CONNECTED TO THE RADIO CHANNELS
-#define RC_CH1_PIN  8
-#define RC_CH2_PIN  7
-#define RC_CH3_PIN  4
-#define RC_CH4_PIN  2
+#define RC_CH1_PIN  16
+#define RC_CH2_PIN  17
+#define RC_CH3_PIN  18
+#define RC_CH4_PIN  19
+#define RC_CH5_PIN  20
+#define RC_CH6_PIN  21
 
 uint16_t rc_values[RC_NUM_CHANNELS];
 uint32_t rc_start[RC_NUM_CHANNELS];
@@ -67,6 +71,12 @@ void calc_ch3() {
 void calc_ch4() {
   calc_input(RC_CH4, RC_CH4_PIN);
 }
+void calc_ch5() {
+  calc_input(RC_CH5, RC_CH5_PIN);
+}
+void calc_ch6() {
+  calc_input(RC_CH6, RC_CH6_PIN);
+}
 
 void setup() {
   Serial.begin(SERIAL_PORT_SPEED);
@@ -75,11 +85,15 @@ void setup() {
   pinMode(RC_CH2_PIN, INPUT);
   pinMode(RC_CH3_PIN, INPUT);
   pinMode(RC_CH4_PIN, INPUT);
+  pinMode(RC_CH5_PIN, INPUT);
+  pinMode(RC_CH6_PIN, INPUT);
 
   enableInterrupt(RC_CH1_PIN, calc_ch1, CHANGE);
   enableInterrupt(RC_CH2_PIN, calc_ch2, CHANGE);
   enableInterrupt(RC_CH3_PIN, calc_ch3, CHANGE);
   enableInterrupt(RC_CH4_PIN, calc_ch4, CHANGE);
+  enableInterrupt(RC_CH5_PIN, calc_ch5, CHANGE);
+  enableInterrupt(RC_CH6_PIN, calc_ch6, CHANGE);
 }
 
 void loop() {
@@ -88,7 +102,9 @@ void loop() {
   Serial.print("CH1:"); Serial.print(rc_values[RC_CH1]); Serial.print("\t");
   Serial.print("CH2:"); Serial.print(rc_values[RC_CH2]); Serial.print("\t");
   Serial.print("CH3:"); Serial.print(rc_values[RC_CH3]); Serial.print("\t");
-  Serial.print("CH4:"); Serial.println(rc_values[RC_CH4]);
+  Serial.print("CH4:"); Serial.print(rc_values[RC_CH4]); Serial.print("\t");
+  Serial.print("CH5:"); Serial.print(rc_values[RC_CH5]); Serial.print("\t");
+  Serial.print("CH6:"); Serial.println(rc_values[RC_CH6]);
 
   delay(200);
 }
